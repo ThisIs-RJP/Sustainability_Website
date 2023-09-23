c4204d8b1e80d64a46223eb3771d72fa42500639
RJ Paraiso <103213491+ThisIs-RJP@users.noreply.github.com>
2023-09-23 22:48:58 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import { useSpring, animated } from 'react-spring';
 import './App.css';
 // import pic from './images/download.png'
 import circles from './images/rotatingCircles.png'
+// import Fade from 'react-reveal/Fade'
+import leaf from './images/leaf.png'
+// const { useState, useEffect } = React;
+import { useState, useEffect } from 'react';
 
 function App() {
-  // const props = useSpring({ opacity: 1, from: { opacity: -10} });
+
+  const [isVisible, setIsVisible] = useState(true);
+  const [height, setHeight] = useState(0)
   const fadeIn = useSpring({
 
     // opacity set 
@@ -16,16 +22,38 @@ function App() {
 
   });
 
-  return (
-    <div className="App">
-      <div className='navBar'>
-        <b><a href="youtube.com" className='link'> Link 1 </a ></b>
-        <div className='gapBox'>
-        </div>
-        <b><a href="youtube.com" className='link'> Link2 </a></b>
+  useEffect(() => {
+    window.addEventListener("scroll", listenToScroll);
+    return () =>
+      window.removeEventListener("scroll", listenToScroll);
+  }, [])
 
-      </div>
+
+  const listenToScroll = () => {
+    let heightToHideFrom = 900;
+    const winScroll = document.body.scrollTop ||
+      document.documentElement.scrollTop;
+    setHeight(winScroll);
+
+    if (winScroll > heightToHideFrom) {
+      isVisible && setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  return (
+
+    <div className="App">
       <animated.div style={fadeIn}>
+        <div className='navBar'>
+          <b><a href="youtube.com" className='link'> Link 1 </a ></b>
+          <div className='gapBox'>
+          </div>
+          <b><a href="youtube.com" className='link'> Link2 </a></b>
+
+        </div>
+        {/* <animated.div style={fadeIn}> */}
         <div className='container1'>
           <div className='textBox1'>
             <h1>
@@ -58,8 +86,12 @@ function App() {
           </div>
           <div className='greenLine'>
           </div>
-
+          <div className='leaf'>
+            <img src={leaf} className='leafP' />
+          </div>
         </div>
+        {/* THIS IS THE END OF THE CONTAINER ############################### */}
+        {/* </animated.div> */}
       </animated.div>
       <br></br>
       <br></br>
@@ -68,9 +100,39 @@ function App() {
       <br></br>
       <br></br>
       <br></br>
-      {/* <div className='circle1'>
-          <img src=
-        </div> */}
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <br></br>
+      <div id="container2">
+        <br></br>
+        <br></br>
+        <br></br>
+        <div id="height">
+          <b>height: {height} - {isVisible ? "show" : "hide"}</b>
+        </div>
+        {
+          isVisible
+          &&
+          <div id="hide">
+            <animated.div>
+              <center>
+                <h1>
+                  This is the new text
+                </h1>
+              </center>
+            </animated.div>
+            <div className='textbox2'>
+              <h1>
+                This is the text box
+              </h1>
+            </div>
+          </div>
+        }
+
+      </div>
     </div >
   );
 }
